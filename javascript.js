06d76d7afa48e583b9692167d5925bc5a6f62c0d
Joe <flightbuzz1234@gmail.com>
2022-05-12 20:39:04 +0100

    Added buttons to change grid size goes up and down, new button to reset to 16x16

--- javascript.js
+++ javascript.js
@@ -1,13 +1,55 @@
 const divContainer = document.querySelector(".div-container");
 const divBox = document.querySelectorAll(".div-box");
+const btn16 = document.getElementById("16");
+const btn20 = document.getElementById("20");
+const btn32 = document.getElementById("32");
+const btn64 = document.getElementById("64");
+const newSheet = document.getElementById("newSheet");
 
-let number = getNumber();
+btn16.addEventListener("click", function (e) {
+  divContainer.replaceChildren();
+  number = 16;
+  modifyStyle(number);
+  divAppend(16 * 16);
+});
+
+btn20.addEventListener("click", function (e) {
+  divContainer.replaceChildren();
+  number = 20;
+  modifyStyle(number);
+  divAppend(20 * 20);
+});
+
+btn32.addEventListener("click", function (e) {
+  divContainer.replaceChildren();
+  number = 32;
+  modifyStyle(number);
+  divAppend(32 * 32);
+});
+btn64.addEventListener("click", function (e) {
+  divContainer.replaceChildren();
+  number = 64;
+  modifyStyle(number);
+  divAppend(64 * 64);
+});
+
+newSheet.addEventListener("click", function (e) {
+  divContainer.replaceChildren();
+  number = 16;
+  modifyStyle(number);
+  divAppend(16 * 16);
+});
+
+// User inputted number
+let number = 16;
 
 function getNumber() {
-  return +prompt("Number? 16, 20 or 32");
+  return (number = +prompt("Number? 16, 20 or 32"));
 }
 
+// Adds divs based on number squared
 divAppend(number * number);
+
 function divAppend(number) {
   for (i = 0; i < number; i++) {
     const newDiv = document.createElement("div");
@@ -16,9 +58,12 @@ function divAppend(number) {
   }
 }
 
+// Changes rows and colums with user number
 function modifyStyle(number) {
   divContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
   divContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
 }
 
-modifyStyle(number);
+// if box button clicked
+//remove all divs
+// call append with new number
