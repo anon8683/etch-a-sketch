26703dd23267b53878b6441cb54fdf6d139be713
Joe <flightbuzz1234@gmail.com>
2022-05-12 18:03:11 +0100

    Added barebones javascript to get it working

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 const divContainer = document.querySelector(".div-container");
 const divBox = document.querySelectorAll(".div-box");
-let number = 0;
+
+let number = getNumber();
+
 function getNumber() {
   return +prompt("Number? 16, 20 or 32");
 }
 
-function divAppend() {
-  number = getNumber();
+divAppend(number * number);
+function divAppend(number) {
   for (i = 0; i < number; i++) {
     const newDiv = document.createElement("div");
     newDiv.className = "div-box";
