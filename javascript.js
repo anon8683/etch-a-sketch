9aaa3c0ea1d08557bf51e5c299884cf1c936d293
Joe <flightbuzz1234@gmail.com>
2022-05-13 05:12:42 +0100

    Added background color picker, reset button will clean it, default white

--- javascript.js
+++ javascript.js
@@ -3,19 +3,26 @@ const divBox = document.querySelectorAll(".div-box");
 const slider = document.getElementById("myRange");
 const sliderDisplay = document.getElementById("grid-size");
 const colorPicker = document.getElementById("color-picker");
+const backgroundColor = document.getElementById("bgcolor-picker");
 const newButton = document.getElementById("newSheet");
+const backGroundButton = document.getElementById("background");
 
 let number = 16;
 let color = "black";
+let backGroundColor = "white";
 
 // Adds listener to slider, runs func when slider changed
 slider.addEventListener("change", getSlider);
 colorPicker.addEventListener("change", getColor);
+backgroundColor.addEventListener("change", changeBackground);
+
 newButton.addEventListener("click", newSheet);
+
 sliderDisplay.textContent = `${number}x${number}`;
 
 function newSheet() {
   clearGrid(number);
+  divContainer.style.backgroundColor = "white";
 }
 
 // Gets the current value of slider and turns number into value
@@ -28,8 +35,13 @@ function getSlider() {
 
 function getColor() {
   color = document.getElementById("color-picker").value;
-
   return color;
+}
+
+function changeBackground() {
+  backGroundColor = document.getElementById("bgcolor-picker").value;
+  divContainer.style.backgroundColor = backGroundColor;
+  return backGroundColor;
 }
 
 // Removes all dom divs, then appends new number of divs and modifys grid column/row
